e8496ce5482bfe7dbd70907bdaf4b1ddc239c0ae
maosiji <code@maosiji.cn>
2024-11-30 22:56:36 +0800

    d

---
+++ d.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * author               : 猫斯基
+ * url                  : maosiji.com
+ * email                : 1394846666@qq.com
+ * wechat               : maosiji-com
+ * date                 : 2024-11-30 22:55
+ * update               : 
+ * project              : library-luphp
+ * official website     : xiaoyingyong.cn
+ * official name        : 小应用商店
+ * official email       : 1211806667@qq.com
+ * official WeChat      : 1211806667
+ * description          : 
+ * read me              : 感谢您使用 小应用商店 的产品。您的支持，是我们最大的动力；您的反对，是我们最大的阻力
+ * remind               ：使用盗版，存在风险；支持正版，将会有跟多的产品与您见面
+ */
