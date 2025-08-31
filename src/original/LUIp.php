e03aec627298a6019c08d87e49c743dcd43d78c1
maosiji <code@maosiji.cn>
2025-08-31 23:58:26 +0800

    增加 LUArray 生成占位符数组

--- src/original/LUIp.php
+++ src/original/LUIp.php
@@ -22,9 +22,9 @@ if (!class_exists('LUIp')) {
 
         /**
          * 获取用户IP
-         * @return array
+         * @return string
          */
-        public function get_visitor_ip()
+        public function get_ip()
         {
             // 优先检查 CDN 或反向代理传来的头信息
             if (!empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {
@@ -50,15 +50,60 @@ if (!class_exists('LUIp')) {
                 // 最后回退到服务器直接获取的 IP
                 $ip = $_SERVER['REMOTE_ADDR'];
             } else {
-                $ip = 'ip 地址未获取到';
+//                $ip = 'ip 地址未获取到';
+                $ip = NULL;
             }
 
             // 再次验证 IP 地址是否合法
-            if (!filter_var($ip, FILTER_VALIDATE_IP)) {
-                $ip = 'ip 地址不合法';
+            if (!$this->is_ip($ip)) {
+//                $ip = 'ip 地址不合法';
+                $ip = NULL;
             }
 
             return $ip;
+        }
+
+        /**
+         * 判断是不是 IPV4 地址
+         * @param string $ip
+         * @return bool
+         */
+        public function is_ipv4(string $ip ): bool
+        {
+            return filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) !== false;
+        }
+
+        /**
+         * 判断是不是 IPV6 地址
+         * @param string $ip
+         * @return bool
+         */
+        public function is_ipv6(string $ip ): bool
+        {
+            return filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) !== false;
+        }
+
+        /**
+         * 判断是不是 IP 地址
+         * @param string $ip
+         * @return bool
+         */
+        public function is_ip(string $ip ): bool
+        {
+            return filter_var($ip, FILTER_VALIDATE_IP) !== false;
+        }
+
+        /**
+         * 判断是不是公网 IP 地址
+         * @param string $ip
+         * @return bool
+         *
+         * FILTER_FLAG_NO_PRIV_RANGE，排除私有地址（如 192.168.x.x, 10.x.x.x, 172.16-31.x.x）
+         * FILTER_FLAG_NO_RES_RANGE，排除保留地址（如 127.x.x.x 回环，169.254.x.x 链路本地，0.0.0.0 等）
+         */
+        public function is_public_ip(string $ip ): bool
+        {
+            return filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE ) !== false;
         }
 
 
